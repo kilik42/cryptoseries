589ac400e9935c388f79bc3a0f6d4f80db090b7b
marvin evins <marvinevins@yahoo.com>
2020-12-25 20:51:02 -0600

    working on ownable operations

--- contracts/Series.sol
+++ contracts/Series.sol
@@ -1,15 +1,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
+import 'openzeppelin-solidity/contracts/ownership/Ownable.sol';
 
-contract Series {
+
+contract Series is Ownable{
   string public title;
   uint public pledgePerEpisode;
   uint public minimumPublicationPeriod;
 
+  // address owner;
+
+  // modifier onlyOwner (){
+  //   require (msg.sender == owner);
+  //   _;
+  // }
 
   constructor(string  memory _title,  uint _pledgePerEpisode, uint minimumPublicationPeriod) public  {
-
+   // owner = msg.sender;
     title = _title;
     pledgePerEpisode = _pledgePerEpisode;
     minimumPublicationPeriod = _minimumPublicationPeriod;
@@ -21,6 +29,16 @@ contract Series {
      
    }
 
+  function withdraw() public {
 
+  }
+
+  function publish(string episodeLink) public onlyOwner{
+    
+  }
+
+  function close() public onlyOwner {
+    
+  }
 
 }
